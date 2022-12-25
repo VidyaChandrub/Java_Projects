841165645314728ff39f59817a9a9e9bd4a8be5d
VidyaChandrub <vidyac266@gmail.com>
2022-12-25 21:34:35 +0530

    sum

---
+++ Examples/Sum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Sum{
+    public static void main(String args[]){
+
+        Scanner sc1 = new Scanner(System.in);
+        Scanner sc2 = new Scanner(System.in);
+
+        int a= sc1.nextInt();
+        int b= sc2.nextInt();
+
+        int c= a + b;
+
+        System.out.println(c);
+    }
+}
