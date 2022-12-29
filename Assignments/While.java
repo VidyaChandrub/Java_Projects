2b4a3c93c6c4904eb9fc1169d04c18645055390b
VidyaChandrub <vidyac266@gmail.com>
2022-12-29 20:39:29 +0530

    odd

--- Assignments/While.java
+++ Assignments/While.java
@@ -25,9 +25,100 @@
     }
 } */
 
-class While{
+/* class While{
     public static void main(String args[]){
+       int temp;
+       int number=524287;
+
+       for(int i=1; i<=number;i++){
+        if(number%2 == 0){
+            temp=2;
+            number=number/2;
+        }
+       }
+
+       if(number%)
         
-       
     }
 }
+ */
+
+/*  class While{
+    public static void main(String args[]){
+        int i=1;
+        while(i<=10){
+            System.out.println(i);
+            i++;
+        }
+    }
+ } */
+
+  /* class While{
+    public static void main(String args[]){
+        int i=10;
+        while(i>=1){
+            System.out.println(i);
+            i--;
+        }
+    }
+ } */
+
+ /* class While{
+    public static void main(String args[]){
+        for(int i=5; i<=15; i++){
+            System.out.println(i);
+        }
+    }
+ } */
+
+ /* class While{
+    public static void main(String args[]){
+        for(int i=15; i>=10; i--){
+            System.out.println(i);
+        }
+    }
+ } */
+
+ /* class While{
+    public static void main(String args[]){
+        int i = 1;
+        while(i <= 10){
+            if(i%2 == 0)
+                System.out.println(i);
+                i++;
+        }
+    }
+ } */
+
+ /* class While{
+    public static void main(String args[]){
+        int i = 2;
+        while(i<=10){
+         System.out.println(i);
+               i= i+2;   
+        }
+
+    }
+ } */
+
+ /* class While{
+    public static void main(String args[]){
+       int i=1;
+       while(i<=10){
+           if(i % 2!=0)
+                continue;
+           System.out.println(i);
+           i++;
+       }
+    }
+ } */
+
+ class While{
+    public static void main(String args[]){
+        for(int i=1; i<=10; i++){
+            if(i%2 == 0)
+            continue;
+            System.out.println(i);
+        }
+    }
+ }
