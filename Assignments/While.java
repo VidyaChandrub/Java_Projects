b9bb32c0dae2c313158108c5e681bbfa02d65d9f
VidyaChandrub <vidyac266@gmail.com>
2022-12-29 21:51:08 +0530

    multiple

--- Assignments/While.java
+++ Assignments/While.java
@@ -113,12 +113,33 @@
     }
  } */
 
- class While{
+ /* class While{
     public static void main(String args[]){
         for(int i=1; i<=10; i++){
             if(i%2 == 0)
             continue;
             System.out.println(i);
+        }
+    }
+ } */
+
+ /* class While{
+    public static void main(String args[]){
+        int i=1;
+        while(i<=10){
+            System.out.println("4 * "+ i + " = " + 4*i);
+            
+            i++;
+        }
+    }
+ } */
+
+ class While{
+    public static void main(String args[]){
+        int i=1;
+        while(i<=10){
+            System.out.println("6 * "+ i + " = " + 6*i);
+            i++;
         }
     }
  }
