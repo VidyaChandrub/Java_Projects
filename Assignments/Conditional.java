d51394f935650f0e111e92f16c026c27f15605f2
VidyaChandrub <vidyac266@gmail.com>
2022-12-29 12:44:22 +0530

    while

--- Assignments/Conditional.java
+++ Assignments/Conditional.java
@@ -139,6 +139,38 @@ import java.util.Scanner;
     }
 } */
 
+/* class Conditional{
+    public static void main(String args[]){
+       Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int temp;
+        if(b<a){
+            temp = a;
+            a=b;
+            b=temp;
+            }
+
+        if(c<b){
+            temp = b;
+            b=c;
+            c=temp;
+            }
+
+        if(b<a){
+            temp = a;
+            a=b;
+            b=temp;
+            }
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+} */
+
 class Conditional{
     public static void main(String args[]){
        Scanner sc = new Scanner(System.in);
@@ -146,16 +178,27 @@ class Conditional{
         int b = sc.nextInt();
         int c = sc.nextInt();
         int temp;
-        if(a<b){
+        if(a>b){
             temp = a;
             a=b;
             b=temp;
             }
 
-        if(b<c){
+        if(b>c){
+            temp = b;
+            b=c;
+            c=temp;
+            }
+
+        if(b>a){
             temp = a;
             a=b;
             b=temp;
             }
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
     }
 }
---
+++ Assignments/While.java
@@ -0,0 +1,33 @@
+/* class While{
+    public static void main(String args[]){
+        int i=1;
+        while(i <= 5){
+            if(i%2==0 && i%3==0 && i%5==0){
+                System.out.println(i);  
+                i++;
+            }
+          
+        }
+       
+    }
+} */
+
+/* class While{
+    public static void main(String args[]){
+        
+        for (int num = 0; num < 100; num++)
+        {    
+            
+            if (num % 3 == 0 && num % 5 == 0)
+                System.out.print(num + " ");
+        }
+       
+    }
+} */
+
+class While{
+    public static void main(String args[]){
+        
+       
+    }
+}
