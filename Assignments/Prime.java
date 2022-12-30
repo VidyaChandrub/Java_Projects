51d66da4e3f832fe22b8db243c048cb94e2ee65d
VidyaChandrub <vidyac266@gmail.com>
2022-12-30 21:53:21 +0530

    Prime

--- Assignments/Prime.java
+++ Assignments/Prime.java
@@ -1,6 +1,21 @@
 class Prime{
     public static void main(String args[]){
-        int num=33,i=2;
+        int num=6,i=2;
         boolean flag = false;
+
+        while(i <= num/2){
+            if(num%2==0){
+                flag =true;
+                break;
+            }
+            i++;
+        }
+
+        if(!flag){
+            System.out.println("Prime");
+            }
+            else{
+             System.out.println("Not Prime");   
+            }
     }
 }
