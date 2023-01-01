3aac73640df3fd70788d79663dfb8466b1e4c494
VidyaChandrub <vidyac266@gmail.com>
2023-01-01 22:21:40 +0530

    0-1 tri

--- Patterns/Floyds.java
+++ Patterns/Floyds.java
@@ -1,8 +1,23 @@
 /* Floyds triangle */
 
+ /*  1
+     2 3
+     4 5 6
+     7 8 9 10
+     11 12 13 14 15 */
+
 class Floyds{
     public static void main(String args[]){
-        
-        for(int i=1; i<=4; i++)
+        int n = 5;
+        int number = 1;
+
+        for(int i=1; i<=n; i++){                       
+
+            for(int j=1; j<=i; j++){
+                System.out.print(number+" ");
+                number++;
+            }
+            System.out.println(" ");
+        }
     }
 }
