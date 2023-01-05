b72e8505d3487a3160c65333b33c3d09736a4021
VidyaChandrub <vidyac266@gmail.com>
2023-01-05 14:34:07 +0530

    arrays

--- Arrays/Practice/Test.java
+++ Arrays/Practice/Test.java
@@ -1,5 +1,26 @@
 import java.util.Scanner;
 
 class Test{
-    
+
+    public static void main(String args[]){
+
+       /*  String[] names = {"Vidya", "Chandru", "Bharu","Sandya","Padma"};
+
+        for(int i=0; i<=names.length; i++){
+            System.out.println(names[i]);
+        } */
+
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+
+        String[] names = new String[size];
+
+        for(int i=0; i<size; i++){
+            names[i] = sc.next();cls
+        }
+
+        for(int i=0; i<size; i++){
+            System.out.println(names[i]);
+        }
+    }
 }
