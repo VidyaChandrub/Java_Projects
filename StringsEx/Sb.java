c8d6d484464f06e2faf7459c68d9f83ac724a9e3
VidyaChandrub <vidyac266@gmail.com>
2023-01-16 19:22:02 +0530

    stringbuilder

--- StringsEx/Sb.java
+++ StringsEx/Sb.java
@@ -3,7 +3,18 @@ class Sb{
 
         StringBuilder sb = new StringBuilder("Vidya");
         System.out.println(sb);
+
         sb.setCharAt(0,'B');
+        System.out.println(sb);
+
+        sb.insert(0,'C');
+        System.out.println(sb);
+
+        sb.insert(3,'y');
+        System.out.println(sb);
+
+        // sb.delete(3,4);
+        sb.delete(3,5);
         System.out.println(sb);
 
     }
