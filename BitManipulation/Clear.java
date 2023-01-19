d742a51d210dabb948aaa312836a08718a393756
VidyaChandrub <vidyac266@gmail.com>
2023-01-19 22:17:57 +0530

    clear bit

---
+++ BitManipulation/Clear.java
@@ -0,0 +1,15 @@
+class Clear{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int clearbit = sc.nextInt();
+
+        int n = 5;
+        int pos = 1;
+        
+        int bitmask = 1 << pos;
+
+        int newNumint newNum = bitMask & num;
+        System.out.println(newNum);
+
+    }
+}
