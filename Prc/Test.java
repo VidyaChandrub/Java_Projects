67e38ddfec544a477bc6cb7abeeffd97b9d22504
VidyaChandrub <vidyac266@gmail.com>
2023-01-28 22:27:42 +0530

    ass

--- Prc/Test.java
+++ Prc/Test.java
@@ -1,13 +1,13 @@
 class Test{
     public static void main(String args[]){
 
-        int number = 501;
-        
-
-        for(int i = 500; i<=600; i++){
+                for(int i = 500; i<=600; i++){
             if(i % 7 == 0){
                 System.out.println(i);
             }
         }
+        
+
+
     }
 }
