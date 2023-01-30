c0a812d164124900879ac94f8b82d28448b5fb99
VidyaChandrub <vidyac266@gmail.com>
2023-01-30 20:18:51 +0530

    arraylist

---
+++ CollectionFramework/ArrayListEx.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class ArrayListEx{
+    public static void main(String args[]){
+
+
+        ArrayList al=new ArrayList();
+        al.add("Vidya");
+        al.add("101");
+        al.add("Prasanna");
+        al.add("Chandana");
+        System.out.println(al);
+    }
+}
